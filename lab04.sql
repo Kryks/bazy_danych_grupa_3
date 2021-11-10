fe9b5f61268daf2f9aa2e87e40f2bc2c1320bc3c
Kryks <92035752+Kryks@users.noreply.github.com>
2021-11-10 16:07:32 +0100

    Update lab04.sql

--- lab04.sql
+++ lab04.sql
@@ -155,3 +155,61 @@ UPDATE postac SET nazwa_statku='Maria' WHERE nazwa LIKE '%a%';
 
 #zadanie 3.2
 UPDATE statek SET max_ladownosc * 0.7; WHERE data_wodowania >= '1601-01-01' and data_wodowania <= '1700-12-31';
+
+#zadanie 3.3
+#zadanie 3.3
+
+SELECT * from postac WHERE wiek>1000;
+
+#zadanie 4.1
+1. Dodajemy nowy rodzaj postaci - wąż (raczej waz lub wonsz)
+2. INSERT INTO ...
+
+ALTER TABLE postac MODIFY  rodzaj enum('wiking','ptak','kobieta','syrena','wonsz');
+
+INSERT INTO postac(id_postaci,nazwa,rodzaj, data_ur,wiek,pesel) VALUES
+(9,'Loko','wonsz','1658-05-02', 15,'12345678910');
+
+#zadanie 4.2
+Sposób 1:
+CREATE TABLE marynarz LIKE postac;
+# tylko struktura, bez danych, odtwarza również klucz główny
+INSERT INTO marynarz SELECT * FROM postac WHERE nazwa_statku is not NULL;
+
+zrobiłem sposob 1
+
+Sposób 2:
+CREATE TABLE marynarz2 SELECT * FROM postac WHERE statek is not NULL;
+# ale nie ma klucza głównego
+ALTER TABLE marynarz2 ADD PRIMARY KEY(id_postaci);
+
+#zadanie 4.3
+c) opcjonalnie należy dodać PRIMARY KEY (jeżeli użyto sposobu nr 2)
+i dodać klucze obce bez względu na wybrany sposób.
+
+Alter table marynarz add FOREIGN KEY (nazwa_statku) REFERENCES statek(nazwa_statku);
+
+#zadania 5
+a)
+
+UPDATE marynarz SET nazwa_statku=NULL WHERE nazwa_statku is not null;
+
+b)
+DELETE FROM postac where nazwa='Ragnar';
+
+c)
+DELETE * FROM statek ;
+
+-
+d)
+
+-
+
+e)
+
+CREATE TABLE zwierz
+(id int auto_increment primary key, 
+nazwa VARCHAR(50),
+wiek INT);
+
+f)
