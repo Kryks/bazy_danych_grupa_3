7223839375ad0126b3c56e8bc09536887c9ce6a0
Kryks <92035752+Kryks@users.noreply.github.com>
2021-11-10 16:08:03 +0100

    Create lab05.sql

---
+++ lab05.sql
@@ -0,0 +1,71 @@
+#LAB 05
+
+#Zadanie 1
+
+#jestesmy na immennej bazie
+
+CREATE TABLE kreatura SELECT * from wikingowie.kreatura;
+
+#aktywna baza wikingowie
+ #zadanie1.1
+CREATE TABLE kryksp.zasob SELECT * from zasob;
+CREATE TABLE kryksp.kreatura SELECT * from kreatura;
+CREATE TABLE kryksp.ekwipunek SELECT * from ekwipunek;
+
+
+#zadanie 1.2
+select * from zasob;
+
+#zadanie 1.3
+select * from zasob where rodzaj='jedzenie';
+#zadanie 1.4
+select idZasobu , nazwa from zasob where idZasobu IN(3,5,7);
+
+#zadanie 2.1
+
+SELECT * from kreatura where rodzaj IN( 'ryba', 'wiking','waz','ptak','malpa') AND udzwig>=50;
+
+SELECT * from kreatura where rodzaj NOT IN( 'wiedzma') AND udzwig>=50;
+#zadanie 2.2
+
+SELECT * from zasob where waga>=2 and waga<=5;
+
+SELECT * from zasob WHERE waga BETWEEN 2 AND 5;
+
+#zadanie 2.3
+SELECT * from kreatura  WHERE waga BETWEEN 30 AND 70 AND nazwa LIKE '%or%'  ;
+
+#zadanie 3.1
+select dataPozyskania from zasob where month(datapozyskania) in (7,8);
+
+select nazwa from zasob where month(datapozyskania) in (7,8);
+
+#zadanie 3.2
+
+select nazwa from zasob where rodzaj is not null order by waga asc;
+
+#zadanie 3.3
+
+select data_Ur , nazwa from krestura order by data_ur asc limit 5;
+
+select data_Ur , nazwa from krestura order by data_ur asc limit 5,3;
+
+#Zadanie 4.1
+
+SELECT rodzaj from kreatura;
+SELECT DISTINCT rodzaj from kreatura;
+SELECT DISTINCT (rodzaj) from kreatura;
+
+#zadanie 4.2
+
+select concat('Ala' , ' ' ,'ma' ,' ', 'kota');
+
+select concat('Zasob ' , nazwa , 'zostal pozyskany w miesiacu o numerze',month(datapozyskania)) from zasob;
+select concat('Zasob ' , nazwa , 'zostal pozyskany w miesiacu o numerze',month(datapozyskania)) as tekst from zasob;
+
+
+#zadanie 4.2
+
+#złącza łancuchy znaków w jeden łańcuch 
+
+metoda concat(kolumna , 'tekst' , funkcja())
