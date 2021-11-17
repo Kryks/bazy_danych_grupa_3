054748bcc9dd25ab0afab7a2acb2e30147811194
Kryks <92035752+Kryks@users.noreply.github.com>
2021-11-17 14:55:55 +0100

    Create lab06.sql

---
+++ lab06.sql
@@ -0,0 +1,13 @@
+#lab 06
+
+#zadanie 1.1
+SELECT avg(waga) from kreatura where rodzaj='wiking';
+
+#zadanie 1.2
+
+SELECT rodzaj,AVG(waga) from kreatura GROUP BY rodzaj;
+
+#zadanie 1.3
+SELECT rodzaj,AVG(waga) from kreatura GROUP BY rodzaj;
+
+#zadanie 1.4
