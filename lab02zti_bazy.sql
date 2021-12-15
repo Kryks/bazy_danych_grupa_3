39d87db60f5ac44f28e04ef2c2ab6772c5498f1c
Kryks <92035752+Kryks@users.noreply.github.com>
2021-12-15 16:12:16 +0100

    Update lab02zti_bazy.sql

--- lab02zti_bazy.sql
+++ lab02zti_bazy.sql
@@ -1,2 +1,10 @@
 #zad1
 select * from pracownik order by nazwisko ASC;
+#zad2
+select imie,nazwisko,pensja,data_urodzenia from pracownik where data_urodzenia>'1979-01-01';
+#zad3
+select * from pracownik where pensja between 3500 AND 5000;
+#zad4
+select * from stan_magazynowy where ilosc>10 ;
+#zad5
+select * from towar where  (nazwa_towaru LIKE'A%' OR nazwa_towaru LIKE'B%' OR nazwa_towaru LIKE  'C%');
