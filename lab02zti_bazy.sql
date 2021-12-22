4078d97c32fe9078b363e91112b074e87f502a76
Kryks <92035752+Kryks@users.noreply.github.com>
2021-12-22 15:50:34 +0100

    Update lab02zti_bazy.sql

--- lab02zti_bazy.sql
+++ lab02zti_bazy.sql
@@ -1,3 +1,4 @@
+Czesc1
 #zad1
 select * from pracownik order by nazwisko ASC;
 #zad2
@@ -9,3 +10,18 @@ select * from stan_magazynowy where ilosc>10 ;
 #zad5
 select * from towar where  (nazwa_towaru LIKE'A%' OR nazwa_towaru LIKE'B%' OR nazwa_towaru LIKE  'C%');
 #zad6
+
+Czesc2
+#zad1
+SELECT imie, nazwisko, nazwa FROM pracownik
+INNER JOIN dzial ON dzial.id_dzialu=pracownik.dzial;
+#zad2
+SELECT nazwa_towaru, nazwa_kategori , ilosc FROM towar
+INNER JOIN kategoria ON kategoria.id_kategori=towar.kategoria
+INNER JOIN stan_magazynowy ON stan_magazynowy.towar=towar.id_towaru
+ORDER BY ilosc DESC;
+#zad3
+SELECT id_zamowienia,status_zamowienia,nazwa_statusu_zamowienia FROM zamowienie
+INNER JOIN status_zamowienia ON status_zamowienia.id_statusu_zamowienia=zamowienie.status_zamowienia
+WHERE nazwa_statusu_zamowienia='anulowane';
+
